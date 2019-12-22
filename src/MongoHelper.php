fa42f1871445d2f202647adc5fd7f2e36368dee0
Kristyna Dankova <kristyna@bocians.net>
2019-12-22 15:21:28 -0800

    command, config and service provider

---
+++ src/MongoHelper.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Deerdama\MongoHelper;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Storage;
+
+class MongoHelper extends Command
+{
+   
+}
--- src/MongoHelperServiceProvider.php
+++ src/MongoHelperServiceProvider.php
@@ -6,5 +6,23 @@ use Illuminate\Support\ServiceProvider;
 
 class MongoHelperServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/mongo_helper.php' => config_path('mongo_helper.php'),
+        ], 'config');
+    }
 
+    public function register()
+    {
+        $this->app->singleton('command.db:mongo-helper', function () {
+            return new ShowOptions;
+        });
+
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/mongo_helper.php', 'config'
+        );
+
+        $this->commands(['command.db:mongo-helper']);
+    }
 }
