732e75ce574d95c81d0fd186c74edc8cfc201fc5
Kristyna Dankova <kristyna@bocians.net>
2019-12-22 19:03:25 -0800

    entire command plus some changes
    - artisan command with most functionalities
    - adjust config file
    - fix service provider

--- src/MongoHelperServiceProvider.php
+++ src/MongoHelperServiceProvider.php
@@ -16,7 +16,7 @@ class MongoHelperServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton('command.db:mongo-helper', function () {
-            return new ShowOptions;
+            return new MongoHelper();
         });
 
         $this->mergeConfigFrom(
